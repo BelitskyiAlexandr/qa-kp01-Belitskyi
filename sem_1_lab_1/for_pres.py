4efa50e6e65e9aa4cc1c472c77715d5507599efc
BelitskyiAlexandr <vilisapett@gmail.com>
2022-11-20 13:39:03 +0200

    bug

--- sem_1_lab_1/for_pres.py
+++ sem_1_lab_1/for_pres.py
@@ -4,12 +4,12 @@ from bufferFile import BufferFile
 from logtextfile import LogTextFile
 
 log = LogTextFile()
-home_dir= Directory("home", log)
+home_dir= Directory("home")
 log.move(home_dir)
 
-dir1 = Directory("dir1", log)
+dir1 = Directory("dir1")
 dir1.move_repository(home_dir)
-dir2 = Directory("dir2", log)
+dir2 = Directory("dir2")
 dir2.move_repository(dir1)
 
 bin1 = BinaryFile(dir1, log)
@@ -39,7 +39,7 @@ print("~~~~" + buf.get_name() +"~~~~")
 print(buf.get_context())
 
 #Dir move
-dir3 = Directory("dir3", log)
+dir3 = Directory("dir3")
 dir3.move_repository(dir2)
 dir2.print_list()
 dir2.move_repository(dir3)
